d1ce6859ea12f545a21cbaf5162c9596ef1b970e
JGirdwood <jessgirdwood@protonmail.com>
2023-09-05 11:28:51 -0700

    config file stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main () {
-    println!("test &c");
+use std::{env, fs, io};
+use serde::Deserialize;
+use toml;
+
+
+#[derive(Debug, Deserialize)]
+struct Config {
+    num: i8,
+    foo: String, 
 }
+
+fn get_xml (s: String) -> String {
+    let out = String::from(s);
+    return out;
+}
+
+fn read_config() -> io::Result<Config> {
+    let content = fs::read_to_string("~/.journal-news")?;
+    Ok(toml::from_str(&content)?)
+}
+
+#[tokio::main]
+async fn main () {
+    let conf = read_config().expect("Could not read settings file");
+    dbg!(conf);
+    let args: Vec<String> = env::args().collect();
+    let action: &String = &args[1];
+    dbg!(action);
+    let test = String::from("test");
+    let xml = get_xml(test);
+    dbg!(xml);
+}
+
