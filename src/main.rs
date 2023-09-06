6f6e830b28877ae2320996cbe5e98e30bd75564d
JGirdwood <jessgirdwood@protonmail.com>
2023-09-05 17:28:53 -0700

    TOML stuff

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,40 @@
-use std::{env, fs, io};
+use std::{env, fs, error::Error};
 use serde::Deserialize;
 use toml;
 
 
 #[derive(Debug, Deserialize)]
+struct Paths {
+    journal_dict: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct List {
+    hours: i8,
+}
+
+#[derive(Deserialize, Debug)]
 struct Config {
-    num: i8,
-    foo: String, 
+    paths: Paths, 
+    list: List,
 }
 
 fn get_xml (s: String) -> String {
     let out = String::from(s);
-    return out;
-}
+        return out;
+    }
 
-fn read_config() -> io::Result<Config> {
-    let content = fs::read_to_string("~/.journal-news")?;
-    Ok(toml::from_str(&content)?)
+fn read_config () -> Result<Config, Box<dyn Error>> {
+    let config: Config = {
+        let config_text = fs::read_to_string("~/.journal-news.toml")?;
+        toml::from_str(&config_text)?;
+    };
+    return config;
 }
 
 #[tokio::main]
-async fn main () {
-    let conf = read_config().expect("Could not read settings file");
+async fn main () -> Result<(), Box<dyn Error>> {
+    let conf = read_config()?;
     dbg!(conf);
     let args: Vec<String> = env::args().collect();
     let action: &String = &args[1];
@@ -29,5 +42,6 @@ async fn main () {
     let test = String::from("test");
     let xml = get_xml(test);
     dbg!(xml);
+    Ok(())
 }
 
