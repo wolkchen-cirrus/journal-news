de3ede4c6994b8c9362e81b5147c0983401dc714
JGirdwood <jessgirdwood@protonmail.com>
2023-09-08 15:33:07 -0700

    changed to json for ease of use

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,19 @@
-use std::{env, fs, error::Error};
-use serde::Deserialize;
-use toml;
+use std::{env, fs, error::Error, path::Path};
+use serde_json::Value;
+use home;
 
-
-#[derive(Debug, Deserialize)]
-struct Paths {
-    journal_dict: String,
-}
-
-#[derive(Debug, Deserialize)]
-struct List {
-    hours: i8,
-}
-
-#[derive(Deserialize, Debug)]
-struct Config {
-    paths: Paths, 
-    list: List,
-}
 
 fn get_xml (s: String) -> String {
     let out = String::from(s);
         return out;
     }
 
-fn read_config () -> Result<Config, Box<dyn Error>> {
-    let config: Config = {
-        let config_text = fs::read_to_string("~/.journal-news.toml")?;
-        toml::from_str(&config_text)?
+fn read_config () -> Result<Value, Box<dyn Error>> {
+    let config = {
+        let hd = home::home_dir().unwrap();
+        let settings_path =  Path::new(&hd).join(".journal-news.json");
+        let config_text = fs::read_to_string(&settings_path)?;
+        serde_json::from_str::<Value>(&config_text)?
     };
     return Ok(config);
 }
@@ -35,13 +21,11 @@ fn read_config () -> Result<Config, Box<dyn Error>> {
 #[tokio::main]
 async fn main () -> Result<(), Box<dyn Error>> {
     let conf = read_config()?;
-    dbg!(conf);
+    println!("{:?}", serde_json::to_string_pretty(&conf));
     let args: Vec<String> = env::args().collect();
     let action: &String = &args[1];
-    dbg!(action);
     let test = String::from("test");
     let xml = get_xml(test);
-    dbg!(xml);
     Ok(())
 }
 
