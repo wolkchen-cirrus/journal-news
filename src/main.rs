0b4aefdca3c5b2cbf1363595e91bb00897a4cd29
JGirdwood <42206925+wolkchen-cirrus@users.noreply.github.com>
2023-09-26 19:25:31 +0100

    fixed json stuff

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-use std::{env, fs, error::Error, path::Path};
+use std::{env, fs, error::Error, path::Path, collections::HashMap};
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use home;
 
+#[derive(Serialize, Deserialize)]
+struct Vars {
+    config: HashMap<String, Value>,
+    urls: HashMap<String, Value>,
+}
 
 fn get_xml (s: String) -> String {
     let out = String::from(s);
         return out;
     }
 
-fn read_config () -> Result<Value, Box<dyn Error>> {
-    let config = {
-        let hd = home::home_dir().unwrap();
-        let settings_path =  Path::new(&hd).join(".journal-news.json");
+fn read_config () -> Result<Vars, Box<dyn Error>> {
+    let hd = home::home_dir().unwrap();
+    let settings_path =  Path::new(&hd).join(".journal-news.json");
+    let config: Vars = {
         let config_text = fs::read_to_string(&settings_path)?;
-        serde_json::from_str::<Value>(&config_text)?
+        serde_json::from_str(&config_text)?
     };
     return Ok(config);
 }
